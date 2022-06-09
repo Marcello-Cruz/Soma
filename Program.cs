246d9dd89349957ca64302599a5d57b4bf4e32fa
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 22:01:40 -0300

    jolisouza e o melhor

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero, segundoNumero, soma;
+
+            Console.WriteLine("Cálculo da soma entre dois números.\n");
+
+            Console.Write("Digite o primeiro número: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            soma = primeiroNumero + segundoNumero;
+
+            Console.WriteLine($"\nSoma: {soma}");
+        }
+    }
+}
